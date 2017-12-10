5771afe6e6fa7fe6fc748c49d2f9b7bf4c2d0c2a
Marko Knöbl <marko.kn@gmail.com>
2017-12-10 22:41:18 +0100

    removing small test function

--- src/app/chess/chess.component.ts
+++ src/app/chess/chess.component.ts
@@ -16,15 +16,4 @@ export class ChessComponent implements OnInit {
 
   ngOnInit() {
   }
-
-  someMovement() {
-    this.game.move(this.game.getPieceByPosition([0, 1]), [0, 3]);
-    this.game.move(this.game.getPieceByPosition([0, 0]), [0, 2]);
-    setTimeout( () => this.game.move(this.game.getPieceByPosition([0, 2]), [1, 2]), 1000);
-    setTimeout( () => this.game.move(this.game.getPieceByPosition([1, 2]), [1, 6]), 2000);
-    setTimeout( () => this.game.move(this.game.getPieceByPosition([1, 0]), [2, 2]), 3000);
-    setTimeout( () => this.game.move(this.game.getPieceByPosition([1, 7]), [2, 5]), 3000);
-    setTimeout( () => this.game.move(this.game.getPieceByPosition([2, 5]), [3, 3]), 4000);
-  }
-
 }
