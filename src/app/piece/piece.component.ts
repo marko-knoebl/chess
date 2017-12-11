10129b9f313285311573f896439ee60e63a422eb
Marko Knöbl <marko.kn@gmail.com>
2017-12-11 13:01:12 +0100

    display symbols for pieces

--- src/app/piece/piece.component.ts
+++ src/app/piece/piece.component.ts
@@ -10,6 +10,15 @@ export class PieceComponent implements OnInit {
   @Input() piece;
   @Input() active;
 
+  symbols = {
+    pawn: '♟',
+    rook: '♜',
+    knight: '♞',
+    bishop: '♝',
+    king: '♚',
+    queen: '♛'
+  };
+
   constructor() { }
 
   ngOnInit() {
